e9676ecfaf0d598fbf2313a4003bcb716a3d38bd
Marc Ramos <allmost79907.@gmail.com>
2023-05-22 11:42:05 -0600

    Probando merge de mback a main

---
+++ app_users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    ci = models.CharField(max_length=200, unique=True, null=True)
+    phone = models.IntegerField(unique=True, null=True)
